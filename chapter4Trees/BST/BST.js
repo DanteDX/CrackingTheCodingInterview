c980166254f38853001a74373ee94cc2af76b673
dantedxwolf <teacher.martin@gmail.com>
2020-11-20 07:59:19 +0600

    BST Implemented with traversals

--- chapter4Trees/BST/BST.js
+++ chapter4Trees/BST/BST.js
@@ -43,40 +43,143 @@ class BST {
     }
   }
   // insert ends here
-  find(value){
-      if(this.root === null) return false;
-      let current = this.root;
-      let found = false;
-      while(current && !found){
-          if(value < current.value){
-            current = current.left;
-            continue;
-          }else if(value > current.value){
-            current = current.right;
-            continue;
-          }else if(value === current.value){
-              found = true;
-          }
+  find(value) {
+    if (this.root === null) return false;
+    let current = this.root;
+    let found = false;
+    while (current && !found) {
+      if (value < current.value) {
+        current = current.left;
+        continue;
+      } else if (value > current.value) {
+        current = current.right;
+        continue;
+      } else if (value === current.value) {
+        found = true;
       }
-      return found;
+    }
+    return found;
   }
 
+
+  /* ######################"Tree Traversal begins here"########################
+  if the tree is more depth, less width, better to use BFS,
+  if the tree is more on width, less depth, better to use DFS */
+
   // breadth first search bst traversal
-  BFS(){
-    if(this.root === null) return undefined;
+  // time coplexity, O(n) as we have to traverse all node
+  // space complexity, O(n)
+  BFS() {
+    if (this.root === null) return undefined;
     let result = [];
     let queue = new Queue();
     queue.enqueue(this.root);
     let current;
-    while(queue.length !== 0){
-        current = queue.dequeue();
-        result.push(current.value);
-        if(current.left) queue.enqueue(current.left);
-        if(current.right) queue.enqueue(current.right);
+    while (queue.length !== 0) {
+      current = queue.dequeue();
+      result.push(current.value);
+      if (current.left) queue.enqueue(current.left);
+      if (current.right) queue.enqueue(current.right);
     }
     return result;
   }
 
+  // all dfs algorithms time complexity O(n), as like b4, we have to traverse every node
+  // all dfs algirthms space complexity O(h), h is the height of the tree
+  //  DFS Preorder can be used for getting the order in which the tree can be created again in the same way
+  DFSPreOrderRecursive(){
+    if (this.root === null) return undefined;
+    let data = [];
+    function traverse(node){
+      data.push(node.value);
+      if(node.left) traverse(node.left);
+      if(node.right) traverse(node.right);
+    }
+    traverse(this.root);
+    return data;
+  }
+
+  DFSPreOrderIterative(){
+    if (this.root === null) return undefined;
+    let data = [];
+    let s = [this.root];
+    let node;
+    while(s.length !== 0){
+      node = s.pop();
+      data.push(node.value);
+      if(node.right) s.push(node.right);
+      if(node.left) s.push(node.left);
+    }
+    return data;
+  }
+
+  // DFS InOrder can be of great used to get minimum to maximum sorted elements from this tree
+  DFSInOrderRecursive(){
+    if (this.root === null) return undefined;
+    let data = [];
+    function traverse(node){
+      if(node.left) traverse(node.left);
+      data.push(node.value);
+      if(node.right) traverse(node.right);
+    }
+    traverse(this.root);
+    return data;
+  }
+
+  DFSInOrderIterative(){
+    if (this.root === null) return undefined;
+    let data = [];
+    let s = [];
+    let node = this.root;
+    while((s.length !== 0) || (node !== null)){
+      if(node !== null){
+        s.push(node);
+        node = node.left;
+      }else{
+        node = s.pop();
+        data.push(node.value);
+        node = node.right;
+      }
+    }
+    return data;
+  }
+
+  DFSPostOrderRecursive(){
+    if (this.root === null) return undefined;
+    let data = [];
+    function traverse(node){
+      if(node.left) traverse(node.left);
+      if(node.right) traverse(node.right);
+      data.push(node.value);
+    }
+    traverse(this.root);
+    return data;
+  }
+
+  DFSPostOrderIterative(){
+    if (this.root === null) return undefined;
+    let data = [];
+    let s = [];
+    let lastNodeVisited = null;
+    let node = this.root;
+    while((s.length !== 0) || (node !== null)){
+      if(node !== null){
+        s.push(node);
+        node = node.left;
+      }else{
+        let peekNode = s[s.length  -1];
+        if(peekNode.right !== null && lastNodeVisited !== peekNode.right){
+          node = peekNode.right;
+        }else{
+          data.push(peekNode.value);
+          lastNodeVisited = s.pop();
+        }
+      }
+    }
+    return data;
+  }
+
+  
 }
 
 module.exports = BST;
--- chapter4Trees/BST/BSTPlayground.js
+++ chapter4Trees/BST/BSTPlayground.js
@@ -8,8 +8,28 @@ bst.insert(10);
 bst.insert(14);
 bst.insert(18);
 bst.insert(23);
-console.log(bst.find(20)); // true
-console.log(bst.find(30)); //false
-console.log(bst.find(17)); //true
-console.log(bst.find(40)); //false
+bst.insert(9);
+bst.insert(11);
+bst.insert(16);
+bst.insert(25);
+// console.log(bst.find(20)); // true
+// console.log(bst.find(30)); //false
+// console.log(bst.find(17)); //true
+// console.log(bst.find(40)); //false
+console.log('BFS result');
 console.log(bst.BFS());
+console.log("################################");
+console.log('PreOrderRecursive result');
+console.log(bst.DFSPreOrderRecursive());
+console.log('PreOrderIterative result');
+console.log(bst.DFSPreOrderIterative());
+console.log("################################");
+console.log('InOrderRecursive result');
+console.log(bst.DFSInOrderRecursive());
+console.log('InOrderIterative result');
+console.log(bst.DFSInOrderIterative());
+console.log("################################");
+console.log('PostOrderRecursive result');
+console.log(bst.DFSPostOrderRecursive());
+console.log('PostOrderIterative result');
+console.log(bst.DFSPostOrderIterative());
